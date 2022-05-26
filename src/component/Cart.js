11313f9ecf7722811a86313baf473e41758ad9ac
Soham De Roy <sohamderoy.iitk@gmail.com>
2022-05-26 15:22:30 +0530

    Update Cart.js

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,34 +1,3 @@
-// import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { addItem, deleteItem } from "../actions/cartAction";
-
-// const Cart = () => {
-//   const state = useSelector((state) => state);
-//   const dispatch = useDispatch();
-//   return (
-//     <div className="cart">
-//       <h2>Number of items in Cart: {state.numOfItems}</h2>
-//       <button
-//         onClick={() => {
-//           dispatch(addItem());
-//         }}
-//       >
-//         Add Item to Cart
-//       </button>
-//       <button
-//         disabled={state.numOfItems > 0 ? false : true}
-//         onClick={() => {
-//           dispatch(deleteItem());
-//         }}
-//       >
-//         Remove Item to Cart
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Cart;
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addItem, deleteItem } from "../actions/cartAction";
